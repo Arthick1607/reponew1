ffef8df4f2d654c70eb8b210326e860b9b3ef932
Arthick1607 <arthickkulkarni@gmail.com>
2023-11-12 18:07:11 +0530

    commit

---
+++ GitCommand/src/main/java/GitCommand/GitCommand/App.java
@@ -0,0 +1,13 @@
+package GitCommand.GitCommand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
